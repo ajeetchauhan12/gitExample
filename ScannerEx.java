170918f319b59fd7f49df7a0aa7441e03a569742
Ajeet <er.ajeet.chauhan@gmail.com>
2021-04-20 13:50:19 +0530

    Scanner ex

---
+++ ScannerEx.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ScannerEx{
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String branch[]){
+
+System.out.println("which is ur branch");
+String st=sc.next();
+System.out.println("My branch is : "+st);
+	}
+
+
+}
